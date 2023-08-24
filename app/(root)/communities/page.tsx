92be6f997c2905df5c360308ec71144edf9f98f4
vinh nguyễn <vinhntse2002@gmail.com>
2023-08-24 18:00:25 +0700

    community

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,3 +1,6 @@
+import CommunityCard from "@/components/card/CommunityCard";
+import SearchBar from "@/components/shared/SearchBar";
+import { fetchCommunities } from "@/lib/actions/community.action";
 import { fetchUser } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -12,7 +15,50 @@ const page = async ({
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
-  return <div>page</div>;
+  
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  })
+  
+  return (
+    <>
+    <h1 className='head-text'>Cộng đồng</h1>
+
+    <div className='mt-5'>
+      <SearchBar routeType='communities' />
+    </div>
+
+    <section className='mt-9 flex flex-wrap gap-4'>
+      {result.communities.length === 0 ? (
+        <p className='no-result'>No Result</p>
+      ) : (
+        <>
+          {result.communities.map((community) => (
+            <CommunityCard
+              key={community.id}
+              id={community.id}
+              name={community.name}
+              username={community.username}
+              imgUrl={community.image}
+              bio={community.bio}
+              members={community.members}
+            />
+          ))}
+        </>
+      )}
+    </section>
+
+    {/* <Pagination
+      path='communities'
+      pageNumber={searchParams?.page ? +searchParams.page : 1}
+      isNext={result.isNext}
+    /> */}
+  </>
+  )
+
+
 };
 
 export default page;
